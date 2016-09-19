66856692826fed92824cd8b3818a96af8f6cc8d0
Maoquan Wang <595163413@qq.com>
2016-09-19 19:47:52 +0800

    fix: bug in data_valid_fns.php
        move "return true" out of the function "foreach()"

--- data_valid_fns.php
+++ data_valid_fns.php
@@ -1,11 +1,10 @@
 <?php
 	function filled_out($form_vars){
 		foreach ($form_vars as $key => $value) {
-			if(!isset($key) || $value=='')
+			if((!isset($key)) || ($value==''))
 				return false;
-			else 
-				return true;
 		}
+        return true;
 	}
 	function valid_email($address){
 		return filter_var($address,FILTER_VALIDATE_EMAIL);
